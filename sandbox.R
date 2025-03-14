e7725ccc25f09b6cd0ca86ce003ff2a88aad8b5e
ampfalaeris <info@microbe.com>
2025-03-14 11:07:49 -0400

    initial commit

---
+++ sandbox.R
@@ -0,0 +1,35 @@
+# Initial Proteomics Analysis file
+# practice working with and analysing proteomics data
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("msqrob2")
+
+library(adegenet)
+library(QFeatures)
+library(plotly)
+library(msqrob2)
+library(limma)
+library(tidyverse)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
